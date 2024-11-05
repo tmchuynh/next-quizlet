5f48d6e75f8d1545140a03ca24f735857a838d62
Tina Huynh <tinamchuynh@gmail.com>
2024-11-05 13:51:52 -0800

    update not found page and install react-icons

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,36 @@
 // app/not-found.tsx
-import Link from 'next/link';
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import styles from "./styles/not-found.module.css";
 
 export default function NotFound() {
+    const [digits, setDigits] = useState( { first: "4", second: "0", third: "4" } );
+
+    useEffect( () => {
+        // Add the CSS class to the document's body
+        document.body.classList.add( styles.notFoundBody );
+
+        // Remove the CSS class when the component unmounts
+        return () => {
+            document.body.classList.remove( styles.notFoundBody );
+        };
+    }, [] );
+
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white">
-            <h1 className="text-6xl font-bold mb-4">404</h1>
-            <p className="text-2xl mb-6">Oops! The page you’re looking for doesn’t exist.</p>
-            <Link href="/" className="text-blue-500 hover:underline text-lg">
-                Go back to Home
-            </Link>
+        <div className={`${ styles.notFoundContainer }`}>
+            <div>
+                <div className={`${ styles.digitContainer }`}>
+                    <span className={styles.digit}>{digits.first}</span>
+                    <span className={styles.digit}>{digits.second}</span>
+                    <span className={styles.digit}>{digits.third}</span>
+                </div>
+                <p className={styles.message}>Oops! The page you're looking for doesn't exist.</p>
+                <Link href="/" className={styles.backHomeLink}>
+                    Go back to Home
+                </Link>
+            </div>
         </div>
     );
 }
