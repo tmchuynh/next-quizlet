7288b00cdbe575535e8145db7c93efc0dbd808a7
Tina Huynh <tinamchuynh@gmail.com>
2024-11-07 17:55:23 -0800

    fix conflicting model with seq

--- src/backend/models/User.ts
+++ src/backend/models/User.ts
@@ -1,19 +1,9 @@
 import { DataTypes, Model, Optional } from 'sequelize';
 import sequelize from '../config/database';
-interface UserAttributes {
-    user_id: string;
-    created_at: Date;
+
+class User extends Model {
+
 }
-
-// Define which attributes are optional when creating a User instance
-type UserCreationAttributes = Optional<UserAttributes, 'created_at'>;
-
-// Define the User model class, extending the Sequelize Model
-class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-    user_id!: string;
-    created_at!: Date;
-}
-
 
 User.init(
     {
